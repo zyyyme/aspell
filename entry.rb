fe9ddc7f7a0ed5fc3e68f74c76b7b68a9a147120
Markus Reiter <me@reitermark.us>
2020-10-22 22:28:00 +0200

    Improve error output.

--- entry.rb
+++ entry.rb
@@ -1,5 +1,7 @@
 #!/usr/bin/env ruby
 
+Warning[:deprecated] = false
+
 require 'open3'
 require 'shellwords'
 require 'strscan'
@@ -24,78 +26,83 @@ def check_file(file, args)
   Open3.popen3('aspell', 'pipe', *args) do |stdin, stdout, stderr, wait_thread|
     errors = []
 
-    extension = File.extname(file)
-    code_block = false
+    begin
+      extension = File.extname(file)
+      code_block = false
 
-    File.open(file, 'r').each_line.with_index do |line, i|
-      if extension == '.tex'
-        if line.match?(/^\s*\\begin{\s*lstlisting\s*}/)
-          code_block = true
-          next
-        elsif line.match?(/^\s*\\end{\s*lstlisting\s*}/)
-          code_block = false
-          next
-        elsif code_block
-          next
-        end
-      end
-
-      stdin.print '^'
-      stdin.print line
-
-      loop do
-        output = stdout.readline
-
-        next if output.start_with?('@(#)')
-        break if output == "\n"
-
-        output = StringScanner.new(output)
-
-        if type = output.scan(/(&|#|\*)/)
-          if type == '*'
-            output.skip(/\n/)
+      File.open(file, 'r').each_line.with_index do |line, i|
+        if extension == '.tex'
+          if line.match?(/^\s*\\begin{\s*lstlisting\s*}/)
+            code_block = true
+            next
+          elsif line.match?(/^\s*\\end{\s*lstlisting\s*}/)
+            code_block = false
+            next
+          elsif code_block
             next
           end
-
-          output.skip(/\ /)
-          word = output.scan(/[^\ \n]+/)
-          output.skip(/\ /)
-          if type == '&'
-            suggestion_count = Integer(output.scan(/\d+/))
-            output.skip(/\ /)
-          else
-            suggestion_count = 0
-          end
-          column = Integer(output.scan(/\d+/))
-
-          suggestions = (0...suggestion_count).map { |i|
-            output.skip(i.zero? ? /:/ : /,/)
-            output.skip(/ /)
-
-            output.scan(/[^,\n]+/)
-          }
-
-          output.skip(/\n/)
-
-          errors << {
-            word: word,
-            line: i + 1,
-            column: column - 1, # https://github.com/GNUAspell/aspell/issues/277
-            suggestions: suggestions,
-          }
         end
 
-        assert_rest(output.rest)
-      end
-    end
+        stdin.print '^'
+        stdin.print line
 
-    stdin.close
+        loop do
+          output = stdout.readline
+
+          next if output.start_with?('@(#)')
+          break if output == "\n"
+
+          output = StringScanner.new(output)
+
+          if type = output.scan(/(&|#|\*)/)
+            if type == '*'
+              output.skip(/\n/)
+              next
+            end
+
+            output.skip(/\ /)
+            word = output.scan(/[^\ \n]+/)
+            output.skip(/\ /)
+            if type == '&'
+              suggestion_count = Integer(output.scan(/\d+/))
+              output.skip(/\ /)
+            else
+              suggestion_count = 0
+            end
+            column = Integer(output.scan(/\d+/))
+
+            suggestions = (0...suggestion_count).map { |i|
+              output.skip(i.zero? ? /:/ : /,/)
+              output.skip(/ /)
+
+              output.scan(/[^,\n]+/)
+            }
+
+            output.skip(/\n/)
+
+            errors << {
+              word: word,
+              line: i + 1,
+              column: column - 1, # https://github.com/GNUAspell/aspell/issues/277
+              suggestions: suggestions,
+            }
+          end
+
+          assert_rest(output.rest)
+        end
+      end
+    ensure
+      stdin.close
+    end
 
     assert_rest(stdout.read)
 
     status = wait_thread.value
     return errors if status.success?
 
+    raise stderr.read
+  rescue EOFError
+    wait_thread.value
     raise stderr.read
   end
 end
@@ -123,7 +130,7 @@ files.each do |file|
     exit_status = 1
   end
 rescue => e
-  puts "::error file=#{escape(file)}::Error: #{e}"
+  puts "::error file=#{escape(file)}::#{e}"
   exit_status = 1
 end
 
