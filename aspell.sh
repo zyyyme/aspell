1897f2d99471bbf2395b3eea2606e6da088eaac0
Markus Reiter <me@reitermark.us>
2022-03-05 22:06:44 +0100

    Update image.

--- aspell.sh
+++ aspell.sh
@@ -2,5 +2,5 @@
 
 set -euo pipefail
 
-docker build -t reitermarkus/aspell "$(dirname "${0}")"
+docker build -t ghcr.io/reitermarkus/aspell:main "$(dirname "${0}")"
 docker run -it --rm -v "${PWD}:${PWD}" -w "${PWD}" reitermarkus/aspell "${@}"
